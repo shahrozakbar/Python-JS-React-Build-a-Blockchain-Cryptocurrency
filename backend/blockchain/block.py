326ab760a9c783861686b2abb23d8e6c8f56245c
Shahroz Akbar Dhillon <shahrozakbar@protonmail.com>
2021-05-19 09:09:53 +0500

    adjust_difficulty(), tests and average_blockrate.py added

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,6 +1,6 @@
 import time
 from backend.util.crypto_hash import crypto_hash
-
+from backend.config import MINE_RATE
 
 GENESIS_DATA = {
     'timestamp': 1,
@@ -45,13 +45,14 @@ class Block:
         """
         timestamp = time.time_ns()
         last_hash = last_block.hash
-        difficulty = last_block.difficulty
+        difficulty = Block.adjust_difficulty(last_block, timestamp)
         nonce = 0
         hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
         while hash[0:difficulty] != '0' * difficulty:
             nonce += 1
             timestamp = time.time_ns()
+            difficulty = Block.adjust_difficulty(last_block, timestamp)
             hash = crypto_hash(timestamp, last_hash, data, difficulty, nonce)
 
 
@@ -64,6 +65,27 @@ class Block:
         :return:
         """
         return Block(**GENESIS_DATA)
+
+    @staticmethod
+    def adjust_difficulty(last_block, new_timestamp):
+        """
+
+        Calculate the adjusted difficulty according to the MINE_RATE.
+        Increase the difficulty for quickly mined blocks.
+        Decrease the Difficulty for slowly mined blocks.
+
+        :param last_block:
+        :param new_timestamp:
+        :return:
+        """
+        if (new_timestamp - last_block.timestamp) < MINE_RATE:
+            return last_block.difficulty + 1
+
+        if (last_block.difficulty - 1) > 0:
+            return last_block.difficulty - 1
+
+        return 1
+
 
 
 def main():
