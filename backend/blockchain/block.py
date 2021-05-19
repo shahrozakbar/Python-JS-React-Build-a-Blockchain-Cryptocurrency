36cbe89a07cc24f8d557ccfd05d693711d61e2d6
Shahroz Akbar Dhillon <shahrozakbar@protonmail.com>
2021-05-19 14:15:44 +0500

    is_valid_block() added

--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -89,15 +89,55 @@ class Block:
         return 1
 
 
+    @staticmethod
+    def is_valid_block(last_block, block):
+        """
+        Validate block by enforcing the following rules:
+            -the block must have the proper last_hash reference
+            -the block must meet the proof of work requirement
+            -the difficulty must only adjust by 1
+            -the block hash mush be a valid combination of the block fields
+        :var
+        """
+
+        if block.last_hash != last_block.hash:
+            raise Exception('The block last_hash must be correct')
+
+        if hex_to_binary(block.hash)[0:block.difficulty] != '0' * block.difficulty:
+            raise Exception('The proof of requirements was not met')
+
+        if (last_block.difficulty - block.difficulty) > 1:
+            raise Exception("The block difficulty must only adjust by 1")
+
+        reconstructed_hash = crypto_hash(
+            block.timestamp,
+            block.last_hash,
+            # block.hash,
+            block.data,
+            block.nonce,
+            block.difficulty
+        )
+
+        if block.hash != reconstructed_hash:
+            raise Exception("the block hash must be correct")
+
+
+
+
+
+
+
 
 def main():
-    # block = Block('foo')
-    # print(block)
-    # print(f'block.py __name__: {__name__}')
-    
     genesis_block = Block.genesis()
-    block = Block.mine_block(genesis_block, 'foo')
-    print(block)
+    bad_block = Block.mine_block(Block.genesis(), 'foo')
+    bad_block.last_hash = 'evil_data'
+
+    try:
+        Block.is_valid_block(genesis_block, bad_block)
+    except Exception as e:
+        print(f'is_valid_block: {e}')
+
 
 if __name__ == '__main__':
     main()
