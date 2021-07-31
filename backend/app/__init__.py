e97e8531bb5742c8e9676fe317bf7dbeaa28f3c0
shahroz <shehrozakbar@gmail.com>
2021-07-31 21:00:54 +0500

    added API transact endpoints

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,11 +1,14 @@
 import os
 import random
-import requests
 
-from flask import Flask, jsonify
+import requests
+from flask import Flask, jsonify, request
 from backend.blockchain.blockchain import Blockchain
+from backend.wallet.transaction import Transaction
+from backend.wallet.wallet import Wallet
 from backend.pubsub import PubSub
 
+wallet = Wallet()
 app = Flask(__name__)
 blockchain = Blockchain()
 pubsub = PubSub(blockchain)
@@ -25,6 +28,22 @@ def route_blockchain_mine():
     block = blockchain.chain[-1]
     pubsub.broadcast_block(block)
     return jsonify(block.to_json())
+
+
+@app.route('/wallet/transact', methods=['POST'])
+def route_wallet_transact():
+    # {'recipient': 'foo', 'amount': 15 }
+    transaction_data = request.get_json()
+    transaction = Transaction(
+        wallet,
+        transaction_data['recipient'],
+        transaction_data['amount']
+    )
+
+    print(f'transaction.to_json(): {transaction.to_json()}')
+    return jsonify(transaction.to_json())
+
+
 
 ROOT_PORT =  5000
 PORT = ROOT_PORT
--- backend/wallet/transaction.py
+++ backend/wallet/transaction.py
@@ -72,6 +72,12 @@ class Transaction:
 
         self.input = self.create_input(sender_wallet, self.output)
 
+    def to_json(self):
+        """
+        Serialize the transaction.
+        :return:
+        """
+        return self.__dict__
 
     @staticmethod
     def is_valid_transaction(transaction):
