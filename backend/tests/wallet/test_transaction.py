ee10e7b2de1041b03c2c3afb8044145fba09bfba
shahroz <shehrozakbar@gmail.com>
2021-07-31 17:35:48 +0500

    added test_transsaction_update_exceeds_balance, test_transaction_update test method to test_transaction.py

--- backend/tests/wallet/test_transaction.py
+++ backend/tests/wallet/test_transaction.py
@@ -27,3 +27,42 @@ def test_transaction():
 def test_transaction_exceeds_balance():
     with pytest.raises(Exception, match='Amount exceeds balance'):
         Transaction(Wallet(), 'recipient', 9001)
+
+def test_transaction_update_exceeds_balance():
+    sender_wallet = Wallet()
+    transaction = Transaction(sender_wallet, 'recipient', 50)
+
+    with pytest.raises(Exception, match='Amount exceeds balance'):
+        transaction.update(sender_wallet, 'new_recipient', 9001)
+
+def test_transaction_update():
+    sender_wallet = Wallet()
+    first_recipient = 'first_recipient'
+    first_amount = 50
+
+    transaction = Transaction(sender_wallet, first_recipient, first_amount)
+
+    next_recipient = 'next_recipient'
+    next_amount = 75
+    transaction.update(sender_wallet, next_recipient, next_amount)
+
+    assert transaction.output[next_recipient] == next_amount
+    assert transaction.output[sender_wallet.address] == \
+           sender_wallet.balance - first_amount - next_amount
+    assert Wallet.verify(
+        transaction.input['public_key'],
+        transaction.output,
+        transaction.input['signature']
+    )
+
+    to_first_again_amount = 25
+    transaction.update(sender_wallet, first_recipient, to_first_again_amount)
+
+    assert transaction.output[first_recipient] == first_amount +  to_first_again_amount
+    assert transaction.output[sender_wallet.address] == \
+           sender_wallet.balance - first_amount - next_amount - to_first_again_amount
+    assert Wallet.verify(
+        transaction.input['public_key'],
+        transaction.output,
+        transaction.input['signature']
+    )
