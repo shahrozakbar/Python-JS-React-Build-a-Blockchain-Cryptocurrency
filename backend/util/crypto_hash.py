6f1e3ed4e8f7393063508570a3088ea8aeb681cb
shahroz <shehrozakbar@gmail.com>
2021-07-31 11:13:23 +0500

    wallets,keys,transactions wallet and crypto module, sign data, verify signatures

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -8,9 +8,7 @@ def crypto_hash(*args):
     """
     stringified_args = sorted(map(lambda data: json.dumps(data), args))
 
-
     joined_data = ''.join(stringified_args)
-
 
     return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -1,9 +1,10 @@
+import json
 import uuid
 from backend.config import STARTING_BALANCE
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives.asymmetric import ec
-
-
+from cryptography.hazmat.primitives import hashes
+from cryptography.exceptions import InvalidSignature
 class Wallet:
     """:var
     An individual wallet for a miner
@@ -21,10 +22,47 @@ class Wallet:
 
         self.public_key = self.private_key.public_key()
 
+    def sign(self, data):
+        """
+        Generate a signature based on the data using the local private key.
+        :param data:
+        :return:
+        """
+        return self.private_key.sign(
+            json.dumps(data).encode('utf-8'),
+            ec.ECDSA(hashes.SHA256())
+        )
+    @staticmethod
+    def verify(public_key, data, signature):
+        """
+        Verify a signature based on the original public key and data.
+        :param public_key:
+        :param data:
+        :param signature:
+        :return:
+        """
+        try:
+            public_key.verify(
+                signature,
+                json.dumps(data).encode('utf-8'),
+                ec.ECDSA(hashes.SHA256())
+            )
+            return True
+        except InvalidSignature:
+            return False
 
 def main():
     wallet = Wallet()
     print(f'wallet.__dict__: {wallet.__dict__}')
+    data = {'foo': 'bar'}
+    signature = wallet.sign(data)
+    print(f'signature: {signature}')
+
+    should_be_valid = Wallet.verify(wallet.public_key, data, signature)
+    print(f'should_be_valid: {should_be_valid}')
+
+    should_be_valid = Wallet.verify(Wallet().public_key, data, signature)
+    print(f'should_be_valid: {should_be_valid}')
 
 
 if __name__ == '__main__':
