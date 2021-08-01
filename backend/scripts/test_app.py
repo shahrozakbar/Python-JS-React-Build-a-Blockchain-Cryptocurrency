735512c7fd7968a1ab2b991a61253317d22ac3e9
shahroz <shehrozakbar@gmail.com>
2021-08-01 22:27:50 +0500

    added tests to test_app.py, getblockchain,get_blockchain_mine,post_wallet_transact methods

---
+++ backend/scripts/test_app.py
@@ -0,0 +1,36 @@
+import time
+
+import requests
+from backend.wallet.wallet import Wallet
+
+BASE_URL = 'http://localhost:5000'
+
+def get_blockchain():
+    return requests.get(f'{BASE_URL}/blockchain').json()
+
+
+def get_blockchain_mine():
+    return requests.get(f'{BASE_URL}/blockchain/mine').json()
+
+
+def post_wallet_transact(recipient, amount):
+    return requests.post(
+        f'{BASE_URL}/wallet/transact',
+        json={'recipient': recipient, 'amount': amount }
+    ).json()
+
+
+start_blockchain = get_blockchain()
+print(f'start_blockchain: {start_blockchain}')
+
+recipient = Wallet().address
+
+post_wallet_transact_1 = post_wallet_transact(recipient, 21)
+print(f'\npost_wallet_transact_1: {post_wallet_transact_1}')
+
+post_wallet_transact_2 = post_wallet_transact(recipient, 13)
+print(f'\npost_wallet_transact_2: {post_wallet_transact_2}')
+
+time.sleep(1)
+mined_block = get_blockchain_mine()
+print(f'\n mined_block: {mined_block}')
