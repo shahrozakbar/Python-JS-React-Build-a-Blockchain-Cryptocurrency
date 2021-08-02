30aebc1d6211ee751a2021d400d631276b932171
shahroz <shehrozakbar@gmail.com>
2021-08-02 09:10:55 +0500

    added calulate_balance method to the wallet module.

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -83,6 +83,31 @@ class Wallet:
             return True
         except InvalidSignature:
             return False
+    @staticmethod
+    def calculate_balance(blockchain, address):
+        """
+        Calculate the balance of the given address considering the transaction
+        data within the blockchain.
+
+        The balance is found by adding the output values that belong to the
+        address since the most recent transaction by that address.
+        :param blockchain:
+        :param address:
+        :return:
+        """
+        balance = STARTING_BALANCE
+
+        for block in blockchain.chain:
+            for transaction in block.data:
+                if transaction['input']['address'] == address:
+                    # Any time the address conducts a new transaction it resets
+                    # its balance
+                    balance = transaction['output'][address]
+                elif address in transaction['output']:
+                    balance += transaction['output'][address]
+
+        return balance
+
 
 def main():
     wallet = Wallet()
