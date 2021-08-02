74c5a49e4a8b2349d377b02afe1e20067e132251
shahroz <shehrozakbar@gmail.com>
2021-08-02 09:31:11 +0500

    added test_calculate_balcne

--- backend/tests/wallet/test_wallet.py
+++ backend/tests/wallet/test_wallet.py
@@ -1,4 +1,8 @@
+from backend.blockchain.blockchain import Blockchain
+from backend.config import STARTING_BALANCE
+from backend.wallet.transaction import Transaction
 from backend.wallet.wallet import Wallet
+
 
 def test_verify_valid_signature():
     data = {'foo': 'test_data'}
@@ -7,9 +11,46 @@ def test_verify_valid_signature():
 
     assert Wallet.verify(wallet.public_key, data, signature)
 
+
 def test_verify_invalid_signatures():
     data = {'foo': 'test_data'}
     wallet = Wallet()
     signature = wallet.sign(data)
 
     assert not Wallet.verify(Wallet().public_key, data, signature)
+
+
+def test_calculate_balance():
+    blockchain = Blockchain()
+    wallet = Wallet()
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) == STARTING_BALANCE
+
+    amount = 50
+    transaction = Transaction(wallet, 'recipient', amount)
+    blockchain.add_block([transaction.to_json()])
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) ==\
+           STARTING_BALANCE - amount
+
+    received_amount_1 = 25
+    received_transaction_1 = Transaction(
+        Wallet(),
+        wallet.address,
+        received_amount_1
+    )
+
+    received_amount_2 = 20
+    received_transaction_2 = Transaction(
+        Wallet(),
+        wallet.address,
+        received_amount_2
+    )
+
+    blockchain.add_block([
+        received_transaction_1.to_json(),
+        received_transaction_2.to_json()
+    ])
+
+    assert Wallet.calculate_balance(blockchain, wallet.address) == \
+        STARTING_BALANCE - amount + received_amount_1 + received_amount_2
