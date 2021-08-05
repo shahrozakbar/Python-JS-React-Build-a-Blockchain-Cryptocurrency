9d7a9cc5179acb32c2f9f53bf894059a68846860
shahroz <shehrozakbar@gmail.com>
2021-08-05 18:06:46 +0500

    frontend init

--- README.md
+++ README.md
@@ -28,3 +28,8 @@ Make sure to activate the virtual environment.
 ```
 export PEER=True && python -m backend.app
 ```
+
+**Run the frontend**
+In the frontend directory:
+
+```npm run start```
---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+
+function App() {
+    const [userQuery, setUserQuery] = useState('');
+
+    return (
+        <div className="App">
+            APP
+        </div>
+    );
+}
+
+export default App;
